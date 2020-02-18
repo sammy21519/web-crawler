6550993e8a1e208c66254851039bbd3f75cb9b70
Sammy, Chiu <47565111+sammy21519@users.noreply.github.com>
2020-02-18 21:24:12 +0800

    Add files via upload

---
+++ web_crawler.py
@@ -0,0 +1,25 @@
+# This Python file uses the following encoding: utf-8
+from bs4 import BeautifulSoup
+import requests
+import time
+import xlwings as xw
+import json
+import os
+from datetime import datetime
+
+
+
+# return a dictionary of wanted info
+def swell_crawler (data_num):
+    
+    res = requests.get("given_api_by_magicseaweed"")
+    data_json=res.json()  
+        
+    return {
+        "localTimestamp" : data_json[(data_num)]["localTimestamp"],
+        "rating" : data_json[(data_num)]["solidRating"],
+        "max_swell" : data_json[(data_num)]["swell"]["maxBreakingHeight"],
+        "min_swell" : data_json[(data_num)]["swell"]["minBreakingHeight"],
+        "period" : data_json[(data_num)]["swell"]["components"]["combined"]["period"],
+        "temperature" : data_json[(data_num)]["condition"]["temperature"],
+    }
